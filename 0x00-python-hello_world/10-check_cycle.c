caed3fa574f64db384036c734510fcb6ec6af0a5
IfiemiE <erigiifiemi@gmail.com>
2022-11-29 05:54:20 +0000

    10 resent

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 
 /**
+  * check_cycle - checks a singly linked list whether there
+  * is a loop/cycle or not
   * @list: a pointer to a (head of a) singly linked list
   *
   * Return: 0 if list has no cycle in it, 1 if there is a cycle
