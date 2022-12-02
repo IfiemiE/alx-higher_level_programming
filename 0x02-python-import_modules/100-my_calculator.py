e476a657da42072a0eef076e431ec93aaba05571
IfiemiE <erigiifiemi@gmail.com>
2022-12-02 20:31:55 +0000

    100-refix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,18 +6,19 @@ if __name__ == "__main__":
     if num_arg != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    operator = sys.argv[2]
-    a = sys.argv[1]
-    b = sys.argv[3]
-    if operator == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        print("Uknown operator. Available operators: +, -, *, and /")
+        operator = sys.argv[2]
+        a = sys.argv[1]
+        b = sys.argv[3]
+        if operator == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif operator == '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif operator == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        elif operator == '/':
+            print("{} / {} = {}".format(a, b, div(a, b)))
+        else:
+            print("Uknown operator. Available operators: +, -, *, and /")
+            exit(1)
         exit(0)
-    exit(0)
