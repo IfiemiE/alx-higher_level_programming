df427f2a8524c22dd586d4a7009fe317b7f541d1
IfiemiE <erigiifiemi@gmail.com>
2022-12-05 23:01:47 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(nRow):
         nCol = len(matrix[i])
         for j in range(nCol):
+            print("{:d}".format(matrix[i][j]), end="")
             if j != (nCol - 1):
-                print("{:d}".format(matrix[i][j]), end=" ")
-            else:
-                print("{:d}".format(matrix[i][j]))
+                print(' ', end='')
+        print('')
