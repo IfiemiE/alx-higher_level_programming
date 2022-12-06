c3932dab09c41c0b9c504b9b8ca2a7f152fa937e
IfiemiE <erigiifiemi@gmail.com>
2022-12-06 04:23:05 +0000

    task 9 fixed

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    N = len(my_list)
-    if N == 0:
-        return
-    max_num = my_list[0]
-    for i in range(1, N + 1):
-        if (max_num < my_list[i]):
-            swap = max_num
-            max_num = my_list[i]
-            my_list[i] = swap
+    max_num = None
+    if my_list:
+        N = len(my_list)
+        if N == 0:
+            return None
+        max_num = my_list[0]
+        for i in range(1, N):
+            if (max_num < my_list[i]):
+                swap = max_num
+                max_num = my_list[i]
+                my_list[i] = swap
 
     return max_num
