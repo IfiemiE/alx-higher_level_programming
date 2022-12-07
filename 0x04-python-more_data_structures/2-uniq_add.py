0ff014fd2df62762ef9f5d54566188f4666ad405
IfiemiE <erigiifiemi@gmail.com>
2022-12-07 14:53:46 +0000

    task 2 retouched

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
+    uniq_sum = 0
     if my_list:
-        uniq_sum = 0
         for i in range(len(my_list)):
             seen = False
             for j in range(0, i):
