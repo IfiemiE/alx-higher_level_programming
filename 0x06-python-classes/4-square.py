22ef28e6c4985443d78dbc09108d4bc3d0893be0
Ifiemi Erigi <erigiifiemi@gmail.com>
2022-12-21 03:29:13 +0000

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Define a Class Square"""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """Initialize a new Square.
+            Args:
+                size (int): The size of the new square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of Square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets/assigns a value to size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
