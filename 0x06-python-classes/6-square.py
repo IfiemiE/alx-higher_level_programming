3da9bbd95823ba9042c332a0b663c020edf5ad5b
Ifiemi Erigi <erigiifiemi@gmail.com>
2022-12-21 23:01:07 +0000

    task 6 touched

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -13,7 +13,7 @@ class Square:
                 position (int, int): The position of the new square.
         """
         self.__size = size
-        self.__size = position
+        self.__position = position
 
     @property
     def size(self):
