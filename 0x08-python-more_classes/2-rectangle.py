9a47630011debe093246682a882846d3d4432201
Ifiemi Erigi <erigiifiemi@gmail.com>
2023-01-05 01:47:53 +0000

    tasks 1 & 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,20 +5,20 @@ Defines a Rectangle class.
 
 
 class Rectangle:
-    """Rectangle class specified by properties: width and height."""
+    """Rectangle class defined by width and height."""
 
     def __init__(self, width=0, height=0):
-        """Creates and initializes a Rectangle instance.
+        """Initializes a Rectangle instance.
         Args:
-            width: width of rectangle
-            height: height of rectangle
+            width: width of the rectangle
+            height: height of the rectangle
         """
         self.width = width
         self.height = height
 
         @property
         def width(self):
-            """Gets the width of a Rectangle instance."""
+            """Retrieves the width of a Rectangle instance."""
             return self.__width
 
         @width.setter
@@ -33,10 +33,9 @@ class Rectangle:
                 raise ValueError("width must be >= 0")
             self.__width = value
 
-
         @property
         def height(self):
-            """Gets the height of a Rectangle instance."""
+            """Retrieves the height of a Rectangle instance."""
             return self.__height
 
         @height.setter
@@ -50,3 +49,19 @@ class Rectangle:
             if value < 0:
                 raise ValueError("height must be >= 0")
             self.__height = value
+
+        def area(self):
+            """Computes the area of a Rectangle
+            Returns:
+                Area of the the rectangle
+            """
+            return self.__width * self.__height
+
+        def perimeter(self):
+            """Calculates the perimeter of a Rectangle instance
+            Returns:
+                Perimeter of the rectangle
+            """
+            if self.__height == 0 or self.__width == 0:
+                return 0
+            return 2 * (self.__width + self.__height)
