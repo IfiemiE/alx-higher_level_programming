ba276a644661e706dacf65d70bc1cee4318bf436
IfiemiE <erigiifiemi@gmail.com>
2023-03-19 21:14:18 +0000

    manipulating result with Python

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" A module demonstrating the
+    the connection to a Database
+    via python module 'MySQLdb'
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    psswd = sys.argv[2]
+    dbase = sys.argv[3]
+    myDb = MySQLdb.connect(host = 'localhost',
+                            user = usr,
+                            password = psswd,
+                            db = dbase,
+                            port = 3306)
+
+    dbCursor = myDb.cursor()
+    result = dbCursor.execute('SELECT * FROM states GROUP BY id')
+    view = dbCursor.fetchall()
+    myDb.close()
+    for i, j in view:
+        if j[0] == 'N':
+            print((i, j))
