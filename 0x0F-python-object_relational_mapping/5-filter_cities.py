8e2f0fa76b31f5ea26b8a61eb1e926d22c40410d
IfiemiE <erigiifiemi@gmail.com>
2023-03-24 21:04:31 +0000

    a variable in a query

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,53 +8,34 @@
 import MySQLdb
 import sys
 
-def filter_cities(state):
-    """filter_cities: prints out cities in a given state
-        Args:
-            state (str): The given state
-        Returns:The query for the selection
-    """
-    query = f"SELECT c.name\
-            FROM cities AS c\
-            INNER JOIN states AS s\
-            WHERE s.name = '{state}' AND c.state_id = s.id\
-            ORDER BY c.id"
-
-    return (query)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     usr = sys.argv[1]
     psswd = sys.argv[2]
     dbase = sys.argv[3]
-    ns = sys.argv[4]
-    alarm = 0
-    for p in range(len(ns)):
-        if (ns[p] == ';') and (p != len(ns)):
-            alarm = 1
-        if (ns[p] != ' ') and alarm == 1:
-            ns = None
-            break
-    if ns != None:
-        myDb = MySQLdb.connect(host = 'localhost',
-                                user = usr,
-                                password = psswd,
-                                db = dbase,
-                                port = 3306)
+    stat = sys.argv[4]
+    for i in stat:
+        if i == ';':
+            stat = None
+    if stat is not None:
+        myDb = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=usr,
+                               password=psswd,
+                               db=dbase)
+
         dbCursor = myDb.cursor()
-
-        Query = filter_cities(ns)
-        dbCursor.execute(Query)
-        view = dbCursor.fetchall()
-        dbCursor.close()
-        myDb.close()
-        count = 0
-        if len(view) != 0:
-            for c in view:
-                count += 1
-                if count != len(view):
-                    print (c[0], end=', ')
-                else:
-                    print(c[0])
-
-        else:
+        cities = dbCursor.execute(f"SELECT c.name\
+                                   FROM cities AS c\
+                                   INNER JOIN states as s\
+                                   WHERE (c.state_id = s.id)\
+                                   AND (s.name = '{stat}')\
+                                   ORDER BY c.id")
+        view_cities = dbCursor.fetchall()
+        if len(view_cities) == 0:
             print(" ")
+        else:
+            for i in range(len(view_cities)):
+                if i == (len(view_cities) - 1):
+                    print(view_cities[i][0])
+                else:
+                    print(view_cities[i][0], end=", ")
