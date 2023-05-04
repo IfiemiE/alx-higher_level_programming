42e67653279ceccb98fa558dea24c05bfb137fa0
IfiemiE <erigiifiemi@gmail.com>
2023-05-04 04:22:26 +0100

    semistandard-compliance fixed

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-	$('DIV#red_header').click(function(){
-    	$(this).css('color', '#FF0000');
-	});
+$(document).ready(function () {
+  $('DIV#red_header').click(function () {
+    $(this).css('color', '#FF0000');
+  });
 });
